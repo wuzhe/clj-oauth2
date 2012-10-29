8655fee3d66f372a3ac950804c20c03c4e3131d4
Anand Prakash <anand.prakash@gmail.com>
2012-10-28 20:27:41 -0700

    steps to move to data.json 1.2.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject timewarrior/clj-oauth2 "0.5.0"
   :description "clj-http and ring middlewares for OAuth 2.0"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.1"]
-                 [clj-http "0.2.6"]]
-  :dev-dependencies [[ring "0.3.11"]
-                     [com.stuartsierra/lazytest "1.1.2"]]
+                 [clj-http "0.5.6"]]
+  :dev-dependencies [[ring "1.1.5"]
+                     [com.stuartsierra/lazytest "1.2.2"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
   :aot [clj-oauth2.OAuth2Exception clj-oauth2.OAuth2StateMismatchException])
